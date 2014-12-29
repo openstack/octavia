847a25042ecfddcb14ca2b62369aaf9eb4699913
ptoohill <phillip.toohill@rackspace.com>
2014-12-29 13:33:20 -0600

    Jinja Haproxy templates
    Adds Jinja templating for haproxy configuration
    Adds tests to verify Jinja templating
    
    Change-Id: I7dd71bec3d4993ffb732dcd316b801498329fd2a
    Partially-Implements: bp/haproxy-amphora-driver

---
+++ octavia/tests/unit/amphorae/drivers/haproxy/jinja/test_jinja_cfg.py
@@ -0,0 +1,312 @@
+# Copyright 2014 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from octavia.amphorae.drivers.haproxy.jinja import jinja_cfg
+from octavia.tests.unit import base as base
+from octavia.tests.unit.common.sample_configs import sample_configs
+
+
+class TestHaproxyCfg(base.TestCase):
+    def setUp(self):
+        super(TestHaproxyCfg, self).setUp()
+        self.jinja_cfg = jinja_cfg.JinjaTemplater(
+            base_amp_path='/var/lib/octavia',
+            base_crt_dir='/listeners')
+
+    def test_get_template(self):
+        template = self.jinja_cfg._get_template()
+        self.assertEqual('haproxy_listener.template', template.name)
+
+    def test_render_template_tls(self):
+        fe = ("frontend sample_listener_id_1\n"
+              "    option tcplog\n"
+              "    maxconn 98\n"
+              "    option forwardfor\n"
+              "    bind 10.0.0.2:443 "
+              "ssl crt /var/lib/octavia/listeners/"
+              "sample_listener_id_1/FakeCN.pem "
+              "crt /var/lib/octavia/listeners/sample_listener_id_1\n"
+              "    mode http\n"
+              "    default_backend sample_pool_id_1\n\n")
+        be = ("backend sample_pool_id_1\n"
+              "    mode http\n"
+              "    balance roundrobin\n"
+              "    redirect scheme https if !{ ssl_fc }\n"
+              "    cookie SRV insert indirect nocache\n"
+              "    timeout check 31\n"
+              "    option httpchk GET /index.html\n"
+              "    http-check expect rstatus 418\n"
+              "    option forwardfor\n"
+              "    server sample_member_id_1 10.0.0.99:82 "
+              "weight 13 check inter 30s fall 3 cookie sample_member_id_1\n"
+              "    server sample_member_id_2 10.0.0.98:82 "
+              "weight 13 check inter 30s fall 3 cookie sample_member_id_2\n\n")
+        tls_tupe = sample_configs.sample_tls_container_tuple(
+            certificate='imaCert1', private_key='imaPrivateKey1',
+            primary_cn='FakeCN')
+        rendered_obj = self.jinja_cfg.render_loadbalancer_obj(
+            sample_configs.sample_listener_tuple(proto='TERMINATED_HTTPS',
+                                                 tls=True, sni=True),
+            tls_tupe)
+        self.assertEqual(
+            sample_configs.sample_base_expected_config(
+                frontend=fe, backend=be),
+            rendered_obj)
+
+    def test_render_template_tls_no_sni(self):
+        fe = ("frontend sample_listener_id_1\n"
+              "    option tcplog\n"
+              "    maxconn 98\n"
+              "    option forwardfor\n"
+              "    bind 10.0.0.2:443 "
+              "ssl crt /var/lib/octavia/listeners/"
+              "sample_listener_id_1/FakeCN.pem\n"
+              "    mode http\n"
+              "    default_backend sample_pool_id_1\n\n")
+        be = ("backend sample_pool_id_1\n"
+              "    mode http\n"
+              "    balance roundrobin\n"
+              "    redirect scheme https if !{ ssl_fc }\n"
+              "    cookie SRV insert indirect nocache\n"
+              "    timeout check 31\n"
+              "    option httpchk GET /index.html\n"
+              "    http-check expect rstatus 418\n"
+              "    option forwardfor\n"
+              "    server sample_member_id_1 10.0.0.99:82 "
+              "weight 13 check inter 30s fall 3 cookie sample_member_id_1\n"
+              "    server sample_member_id_2 10.0.0.98:82 "
+              "weight 13 check inter 30s fall 3 cookie sample_member_id_2\n\n")
+        rendered_obj = self.jinja_cfg.render_loadbalancer_obj(
+            sample_configs.sample_listener_tuple(
+                proto='TERMINATED_HTTPS', tls=True),
+            tls_cert=sample_configs.sample_tls_container_tuple(
+                certificate='ImAalsdkfjCert',
+                private_key='ImAsdlfksdjPrivateKey',
+                primary_cn="FakeCN"))
+        self.assertEqual(
+            sample_configs.sample_base_expected_config(
+                frontend=fe, backend=be),
+            rendered_obj)
+
+    def test_render_template_http(self):
+        be = ("backend sample_pool_id_1\n"
+              "    mode http\n"
+              "    balance roundrobin\n"
+              "    cookie SRV insert indirect nocache\n"
+              "    timeout check 31\n"
+              "    option httpchk GET /index.html\n"
+              "    http-check expect rstatus 418\n"
+              "    option forwardfor\n"
+              "    server sample_member_id_1 10.0.0.99:82 "
+              "weight 13 check inter 30s fall 3 cookie sample_member_id_1\n"
+              "    server sample_member_id_2 10.0.0.98:82 "
+              "weight 13 check inter 30s fall 3 cookie sample_member_id_2\n\n")
+        rendered_obj = self.jinja_cfg.render_loadbalancer_obj(
+            sample_configs.sample_listener_tuple())
+        self.assertEqual(
+            sample_configs.sample_base_expected_config(backend=be),
+            rendered_obj)
+
+    def test_render_template_https(self):
+        fe = ("frontend sample_listener_id_1\n"
+              "    option tcplog\n"
+              "    maxconn 98\n"
+              "    bind 10.0.0.2:443\n"
+              "    mode tcp\n"
+              "    default_backend sample_pool_id_1\n\n")
+        be = ("backend sample_pool_id_1\n"
+              "    mode tcp\n"
+              "    balance roundrobin\n"
+              "    cookie SRV insert indirect nocache\n"
+              "    timeout check 31\n"
+              "    option httpchk GET /index.html\n"
+              "    http-check expect rstatus 418\n"
+              "    option ssl-hello-chk\n"
+              "    server sample_member_id_1 10.0.0.99:82 "
+              "weight 13 check inter 30s fall 3 cookie sample_member_id_1\n"
+              "    server sample_member_id_2 10.0.0.98:82 "
+              "weight 13 check inter 30s fall 3 cookie sample_member_id_2\n\n")
+        rendered_obj = self.jinja_cfg.render_loadbalancer_obj(
+            sample_configs.sample_listener_tuple(proto='HTTPS'))
+        self.assertEqual(sample_configs.sample_base_expected_config(
+            frontend=fe, backend=be), rendered_obj)
+
+    def test_render_template_no_monitor_http(self):
+        be = ("backend sample_pool_id_1\n"
+              "    mode http\n"
+              "    balance roundrobin\n"
+              "    cookie SRV insert indirect nocache\n"
+              "    option forwardfor\n"
+              "    server sample_member_id_1 10.0.0.99:82 weight 13 "
+              "cookie sample_member_id_1\n"
+              "    server sample_member_id_2 10.0.0.98:82 weight 13 "
+              "cookie sample_member_id_2\n\n")
+        rendered_obj = self.jinja_cfg.render_loadbalancer_obj(
+            sample_configs.sample_listener_tuple(proto='HTTP', monitor=False))
+        self.assertEqual(sample_configs.sample_base_expected_config(
+            backend=be), rendered_obj)
+
+    def test_render_template_no_monitor_https(self):
+        fe = ("frontend sample_listener_id_1\n"
+              "    option tcplog\n"
+              "    maxconn 98\n"
+              "    bind 10.0.0.2:443\n"
+              "    mode tcp\n"
+              "    default_backend sample_pool_id_1\n\n")
+        be = ("backend sample_pool_id_1\n"
+              "    mode tcp\n"
+              "    balance roundrobin\n"
+              "    cookie SRV insert indirect nocache\n"
+              "    server sample_member_id_1 10.0.0.99:82 weight 13 "
+              "cookie sample_member_id_1\n"
+              "    server sample_member_id_2 10.0.0.98:82 weight 13 "
+              "cookie sample_member_id_2\n\n")
+        rendered_obj = self.jinja_cfg.render_loadbalancer_obj(
+            sample_configs.sample_listener_tuple(proto='HTTPS', monitor=False))
+        self.assertEqual(sample_configs.sample_base_expected_config(
+            frontend=fe, backend=be), rendered_obj)
+
+    def test_render_template_no_persistence_https(self):
+        fe = ("frontend sample_listener_id_1\n"
+              "    option tcplog\n"
+              "    maxconn 98\n"
+              "    bind 10.0.0.2:443\n"
+              "    mode tcp\n"
+              "    default_backend sample_pool_id_1\n\n")
+        be = ("backend sample_pool_id_1\n"
+              "    mode tcp\n"
+              "    balance roundrobin\n"
+              "    server sample_member_id_1 10.0.0.99:82 weight 13\n"
+              "    server sample_member_id_2 10.0.0.98:82 weight 13\n\n")
+        rendered_obj = self.jinja_cfg.render_loadbalancer_obj(
+            sample_configs.sample_listener_tuple(proto='HTTPS', monitor=False,
+                                                 persistence=False))
+        self.assertEqual(sample_configs.sample_base_expected_config(
+            frontend=fe, backend=be), rendered_obj)
+
+    def test_render_template_no_persistence_http(self):
+        be = ("backend sample_pool_id_1\n"
+              "    mode http\n"
+              "    balance roundrobin\n"
+              "    option forwardfor\n"
+              "    server sample_member_id_1 10.0.0.99:82 weight 13\n"
+              "    server sample_member_id_2 10.0.0.98:82 weight 13\n\n")
+        rendered_obj = self.jinja_cfg.render_loadbalancer_obj(
+            sample_configs.sample_listener_tuple(proto='HTTP', monitor=False,
+                                                 persistence=False))
+        self.assertEqual(sample_configs.sample_base_expected_config(
+            backend=be), rendered_obj)
+
+    def test_render_template_sourceip_persistence(self):
+        be = ("backend sample_pool_id_1\n"
+              "    mode http\n"
+              "    balance roundrobin\n"
+              "    stick-table type ip size 10k\n"
+              "    stick on src\n"
+              "    timeout check 31\n"
+              "    option httpchk GET /index.html\n"
+              "    http-check expect rstatus 418\n"
+              "    option forwardfor\n"
+              "    server sample_member_id_1 10.0.0.99:82 "
+              "weight 13 check inter 30s fall 3\n"
+              "    server sample_member_id_2 10.0.0.98:82 "
+              "weight 13 check inter 30s fall 3\n\n")
+        rendered_obj = self.jinja_cfg.render_loadbalancer_obj(
+            sample_configs.sample_listener_tuple(
+                persistence_type='SOURCE_IP'))
+        self.assertEqual(
+            sample_configs.sample_base_expected_config(backend=be),
+            rendered_obj)
+
+    def test_transform_session_persistence(self):
+        in_persistence = sample_configs.sample_session_persistence_tuple()
+        ret = self.jinja_cfg._transform_session_persistence(in_persistence)
+        self.assertEqual(sample_configs.RET_PERSISTENCE, ret)
+
+    def test_transform_health_monitor(self):
+        in_persistence = sample_configs.sample_health_monitor_tuple()
+        ret = self.jinja_cfg._transform_health_monitor(in_persistence)
+        self.assertEqual(sample_configs.RET_MONITOR, ret)
+
+    def test_transform_member(self):
+        in_member = sample_configs.sample_member_tuple('sample_member_id_1',
+                                                       '10.0.0.99')
+        ret = self.jinja_cfg._transform_member(in_member)
+        self.assertEqual(sample_configs.RET_MEMBER_1, ret)
+
+    def test_transform_pool(self):
+        in_pool = sample_configs.sample_pool_tuple()
+        ret = self.jinja_cfg._transform_pool(in_pool)
+        self.assertEqual(sample_configs.RET_POOL, ret)
+
+    def test_transform_listener(self):
+        in_listener = sample_configs.sample_listener_tuple()
+        ret = self.jinja_cfg._transform_listener(in_listener, None)
+        self.assertEqual(sample_configs.RET_LISTENER, ret)
+
+    def test_transform_loadbalancer(self):
+        in_listener = sample_configs.sample_listener_tuple()
+        ret = self.jinja_cfg._transform_loadbalancer(
+            in_listener.loadbalancer, in_listener, None)
+        self.assertEqual(sample_configs.RET_LB, ret)
+
+    def test_include_member(self):
+        ret = self.jinja_cfg._include_member(
+            sample_configs.sample_member_tuple('sample_member_id_1',
+                                               '10.0.0.99'))
+        self.assertTrue(ret)
+
+    def test_include_member_invalid_status(self):
+        ret = self.jinja_cfg._include_member(
+            sample_configs.sample_member_tuple('sample_member_id_1',
+                                               '10.0.0.99',
+                                               operating_status='PENDING'))
+        self.assertFalse(ret)
+
+    def test_include_member_invalid_admin_state(self):
+        ret = self.jinja_cfg._include_member(
+            sample_configs.sample_member_tuple('sample_member_id_1',
+                                               '10.0.0.99',
+                                               enabled=False))
+        self.assertFalse(ret)
+
+    def test_expand_expected_codes(self):
+        exp_codes = ''
+        self.assertEqual(self.jinja_cfg._expand_expected_codes(exp_codes),
+                         set([]))
+        exp_codes = '200'
+        self.assertEqual(
+            self.jinja_cfg._expand_expected_codes(exp_codes), set(['200']))
+        exp_codes = '200, 201'
+        self.assertEqual(self.jinja_cfg._expand_expected_codes(exp_codes),
+                         set(['200', '201']))
+        exp_codes = '200, 201,202'
+        self.assertEqual(self.jinja_cfg._expand_expected_codes(exp_codes),
+                         set(['200', '201', '202']))
+        exp_codes = '200-202'
+        self.assertEqual(self.jinja_cfg._expand_expected_codes(exp_codes),
+                         set(['200', '201', '202']))
+        exp_codes = '200-202, 205'
+        self.assertEqual(self.jinja_cfg._expand_expected_codes(exp_codes),
+                         set(['200', '201', '202', '205']))
+        exp_codes = '200, 201-203'
+        self.assertEqual(self.jinja_cfg._expand_expected_codes(exp_codes),
+                         set(['200', '201', '202', '203']))
+        exp_codes = '200, 201-203, 205'
+        self.assertEqual(self.jinja_cfg._expand_expected_codes(exp_codes),
+                         set(['200', '201', '202', '203', '205']))
+        exp_codes = '201-200, 205'
+        self.assertEqual(
+            self.jinja_cfg._expand_expected_codes(exp_codes), set(['205']))
---
+++ octavia/tests/unit/common/sample_configs/sample_configs.py
@@ -0,0 +1,308 @@
+# Copyright 2014 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+import collections
+
+RET_PERSISTENCE = {
+    'type': 'HTTP_COOKIE',
+    'cookie_name': 'HTTP_COOKIE'}
+
+RET_MONITOR = {
+    'id': 'sample_monitor_id_1',
+    'type': 'HTTP',
+    'delay': 30,
+    'timeout': 31,
+    'fall_threshold': 3,
+    'http_method': 'GET',
+    'url_path': '/index.html',
+    'expected_codes': '418',
+    'enabled': True}
+
+RET_MEMBER_1 = {
+    'id': 'sample_member_id_1',
+    'address': '10.0.0.99',
+    'protocol_port': 82,
+    'weight': 13,
+    'subnet_id': '10.0.0.1/24',
+    'enabled': True,
+    'operating_status': 'ACTIVE'}
+
+RET_MEMBER_2 = {
+    'id': 'sample_member_id_2',
+    'address': '10.0.0.98',
+    'protocol_port': 82,
+    'weight': 13,
+    'subnet_id': '10.0.0.1/24',
+    'enabled': True,
+    'operating_status': 'ACTIVE'}
+
+RET_POOL = {
+    'id': 'sample_pool_id_1',
+    'protocol': 'http',
+    'lb_algorithm': 'roundrobin',
+    'members': [RET_MEMBER_1, RET_MEMBER_2],
+    'health_monitor': RET_MONITOR,
+    'session_persistence': RET_PERSISTENCE,
+    'enabled': True,
+    'operating_status': 'ACTIVE'}
+
+RET_DEF_TLS_CONT = {'id': 'cont_id_1', 'allencompassingpem': 'imapem',
+                    'primary_cn': 'FakeCn'}
+RET_SNI_CONT_1 = {'id': 'cont_id_2', 'allencompassingpem': 'imapem2',
+                  'primary_cn': 'FakeCn'}
+RET_SNI_CONT_2 = {'id': 'cont_id_3', 'allencompassingpem': 'imapem3',
+                  'primary_cn': 'FakeCn2'}
+
+RET_LISTENER = {
+    'id': 'sample_listener_id_1',
+    'protocol_port': '80',
+    'protocol': 'HTTP',
+    'protocol_mode': 'http',
+    'default_pool': RET_POOL,
+    'connection_limit': 98}
+
+RET_LISTENER_TLS = {
+    'id': 'sample_listener_id_1',
+    'protocol_port': '443',
+    'protocol': 'TERMINATED_HTTPS',
+    'protocol_mode': 'http',
+    'default_pool': RET_POOL,
+    'connection_limit': 98,
+    'tls_container_id': 'cont_id_1',
+    'default_tls_path': '/etc/ssl/sample_loadbalancer_id_1/fakeCN.pem',
+    'default_tls_container': RET_DEF_TLS_CONT}
+
+RET_LISTENER_TLS_SNI = {
+    'id': 'sample_listener_id_1',
+    'protocol_port': '443',
+    'protocol': 'http',
+    'protocol': 'TERMINATED_HTTPS',
+    'default_pool': RET_POOL,
+    'connection_limit': 98,
+    'tls_container_id': 'cont_id_1',
+    'default_tls_path': '/etc/ssl/sample_loadbalancer_id_1/fakeCN.pem',
+    'default_tls_container': RET_DEF_TLS_CONT,
+    'crt_dir': '/v2/sample_loadbalancer_id_1',
+    'sni_container_ids': ['cont_id_2', 'cont_id_3'],
+    'sni_containers': [RET_SNI_CONT_1, RET_SNI_CONT_2]}
+
+RET_LB = {
+    'name': 'test-lb',
+    'vip_address': '10.0.0.2',
+    'listener': RET_LISTENER}
+
+RET_LB_TLS = {
+    'name': 'test-lb',
+    'vip_address': '10.0.0.2',
+    'listener': RET_LISTENER_TLS}
+
+RET_LB_TLS_SNI = {
+    'name': 'test-lb',
+    'vip_address': '10.0.0.2',
+    'listener': RET_LISTENER_TLS_SNI}
+
+
+def sample_loadbalancer_tuple(proto=None, monitor=True, persistence=True,
+                              persistence_type=None, tls=False, sni=False):
+    proto = 'HTTP' if proto is None else proto
+    in_lb = collections.namedtuple(
+        'loadbalancer', 'id, name, protocol, vip, '
+                        'listeners')
+    return in_lb(
+        id='sample_loadbalancer_id_1',
+        name='test-lb',
+        protocol=proto,
+        vip=sample_vip_tuple(),
+        listeners=[sample_listener_tuple(proto=proto, monitor=monitor,
+                                         persistence=persistence,
+                                         persistence_type=persistence_type,
+                                         tls=tls,
+                                         sni=sni)]
+    )
+
+
+def sample_listener_loadbalancer_tuple(proto=None):
+    proto = 'HTTP' if proto is None else proto
+    in_lb = collections.namedtuple(
+        'loadbalancer', 'id, name, protocol, vip')
+    return in_lb(
+        id='sample_loadbalancer_id_1',
+        name='test-lb',
+        protocol=proto,
+        vip=sample_vip_tuple()
+    )
+
+
+def sample_vip_tuple():
+    vip = collections.namedtuple('vip', 'ip_address')
+    return vip(ip_address='10.0.0.2')
+
+
+def sample_listener_tuple(proto=None, monitor=True, persistence=True,
+                          persistence_type=None, tls=False, sni=False):
+    proto = 'HTTP' if proto is None else proto
+    port = '443' if proto is 'HTTPS' or proto is 'TERMINATED_HTTPS' else '80'
+    in_listener = collections.namedtuple(
+        'listener', 'id, protocol_port, protocol, default_pool, '
+                    'connection_limit, tls_container_id, '
+                    'sni_container_ids, default_tls_container, '
+                    'sni_containers, loadbalancer')
+    return in_listener(
+        id='sample_listener_id_1',
+        protocol_port=port,
+        protocol=proto,
+        loadbalancer=sample_listener_loadbalancer_tuple(proto=proto),
+        default_pool=sample_pool_tuple(
+            proto=proto, monitor=monitor, persistence=persistence,
+            persistence_type=persistence_type),
+        connection_limit=98,
+        tls_container_id='cont_id_1' if tls else '',
+        sni_container_ids=['cont_id_2', 'cont_id_3'] if sni else [],
+        default_tls_container=sample_tls_container_tuple(
+            id='cont_id_1', certificate='--imapem1--\n',
+            private_key='--imakey1--\n', intermediates=[
+                '--imainter1--\n', '--imainter1too--\n'],
+            primary_cn='aFakeCN'
+        ) if tls else '',
+        sni_containers=[
+            sample_tls_sni_container_tuple(
+                tls_container=sample_tls_container_tuple(
+                    id='cont_id_2', certificate='--imapem2--\n',
+                    private_key='--imakey2--\n', intermediates=[
+                        '--imainter2--\n', '--imainter2too--\n'
+                    ], primary_cn='aFakeCN')),
+            sample_tls_sni_container_tuple(
+                tls_container=sample_tls_container_tuple(
+                    id='cont_id_3', certificate='--imapem3--\n',
+                    private_key='--imakey3--\n', intermediates=[
+                        '--imainter3--\n', '--imainter3too--\n'
+                    ], primary_cn='aFakeCN'))]
+        if sni else []
+    )
+
+
+def sample_tls_sni_container_tuple(tls_container=None):
+    sc = collections.namedtuple('sni_container', 'tls_container')
+    return sc(tls_container=tls_container)
+
+
+def sample_tls_sni_containers_tuple(tls_container=None):
+    sc = collections.namedtuple('sni_containers', 'tls_container')
+    return [sc(tls_container=tls_container)]
+
+
+def sample_tls_container_tuple(id='cont_id_1', certificate=None,
+                               private_key=None, intermediates=[],
+                               primary_cn=None):
+    sc = collections.namedtuple(
+        'tls_container',
+        'id, certificate, private_key, intermediates, primary_cn')
+    return sc(id=id, certificate=certificate, private_key=private_key,
+              intermediates=intermediates, primary_cn=primary_cn)
+
+
+def sample_pool_tuple(proto=None, monitor=True, persistence=True,
+                      persistence_type=None):
+    proto = 'HTTP' if proto is None else proto
+    in_pool = collections.namedtuple(
+        'pool', 'id, protocol, lb_algorithm, members, healthmonitor,'
+                'sessionpersistence, enabled, operating_status')
+    mon = sample_health_monitor_tuple(proto=proto) if monitor is True else None
+    persis = sample_session_persistence_tuple(
+        persistence_type=persistence_type) if persistence is True else None
+    return in_pool(
+        id='sample_pool_id_1',
+        protocol=proto,
+        lb_algorithm='ROUND_ROBIN',
+        members=[sample_member_tuple('sample_member_id_1', '10.0.0.99'),
+                 sample_member_tuple('sample_member_id_2', '10.0.0.98')],
+        healthmonitor=mon,
+        sessionpersistence=persis,
+        enabled=True,
+        operating_status='ACTIVE')
+
+
+def sample_member_tuple(id, ip, enabled=True, operating_status='ACTIVE'):
+    in_member = collections.namedtuple('member',
+                                       'id, ip_address, protocol_port, '
+                                       'weight, subnet_id, '
+                                       'enabled, operating_status')
+    return in_member(
+        id=id,
+        ip_address=ip,
+        protocol_port=82,
+        weight=13,
+        subnet_id='10.0.0.1/24',
+        enabled=enabled,
+        operating_status=operating_status)
+
+
+def sample_session_persistence_tuple(persistence_type=None):
+    spersistence = collections.namedtuple('SessionPersistence',
+                                          'type, cookie_name')
+    pt = 'HTTP_COOKIE' if persistence_type is None else persistence_type
+    return spersistence(type=pt,
+                        cookie_name=pt)
+
+
+def sample_health_monitor_tuple(proto='HTTP'):
+    proto = 'HTTP' if proto is 'TERMINATED_HTTPS' else proto
+    monitor = collections.namedtuple(
+        'monitor', 'id, type, delay, timeout, fall_threshold, http_method, '
+                   'url_path, expected_codes, enabled')
+
+    return monitor(id='sample_monitor_id_1', type=proto, delay=30,
+                   timeout=31, fall_threshold=3, http_method='GET',
+                   url_path='/index.html', expected_codes='418',
+                   enabled=True)
+
+
+def sample_base_expected_config(frontend=None, backend=None):
+    if frontend is None:
+        frontend = ("frontend sample_listener_id_1\n"
+                    "    option tcplog\n"
+                    "    maxconn 98\n"
+                    "    option forwardfor\n"
+                    "    bind 10.0.0.2:80\n"
+                    "    mode http\n"
+                    "    default_backend sample_pool_id_1\n\n")
+    if backend is None:
+        backend = ("backend sample_pool_id_1\n"
+                   "    mode http\n"
+                   "    balance roundrobin\n"
+                   "    cookie SRV insert indirect nocache\n"
+                   "    timeout check 31\n"
+                   "    option httpchk GET /index.html\n"
+                   "    http-check expect rstatus 418\n"
+                   "    server sample_member_id_1 10.0.0.99:82 weight 13 "
+                   "check inter 30s fall 3 cookie sample_member_id_1\n"
+                   "    server sample_member_id_2 10.0.0.98:82 weight 13 "
+                   "check inter 30s fall 3 cookie sample_member_id_2\n")
+    return ("# Configuration for test-lb\n"
+            "global\n"
+            "    daemon\n"
+            "    user nobody\n"
+            "    group nogroup\n"
+            "    log /dev/log local0\n"
+            "    log /dev/log local1 notice\n"
+            "    stats socket /var/lib/octavia/sample_listener_id_1.sock"
+            " mode 0666 level user\n\n"
+            "defaults\n"
+            "    log global\n"
+            "    retries 3\n"
+            "    option redispatch\n"
+            "    timeout connect 5000\n"
+            "    timeout client 50000\n"
+            "    timeout server 50000\n\n" + frontend + backend)
