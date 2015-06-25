16ba7833911ea767eab19e9757e64634a3f48aab
minwang <swiftwangster@gmail.com>
2015-06-25 15:57:42 -0700

    Add webserver in octavia/devstack/samples
    Needs to add this file so vagrant script can run.
    
    Change-Id: Iba28a75a9ba201d2a22594d9cb8ab7d4014360f7

---
+++ devstack/samples/webserver.sh
@@ -0,0 +1,6 @@
+#!/bin/ash
+
+MYIP=$(/sbin/ifconfig eth0|grep 'inet addr'|awk -F: '{print $2}'| awk '{print $1}');
+while true; do
+    echo -e "HTTP/1.0 200 OK\r\n\r\nWelcome to $MYIP" | sudo nc -l -p 80
+done
