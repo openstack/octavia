9cd07f2e8680d387f3498c848d512f5175e9ca6a
Brandon Logan <brandon.logan@rackspace.com>
2015-08-03 18:04:50 -0500

    Fix member creation when retrieving network
    When a member is created there must be some calculation to determine what
    networks the amphorae need to be plugged into.  This calculation needs to
    retrieve network information.  How it retrieves information was based on an
    old version of the network driver and was missed during the network driver
    decoupling refactor.  The calculate delta method now calls the get_subnet
    method of the member's subnet_id attribute.
    
    Change-Id: I65da3ae5f02b991fb6aad5a267dc02b0572b35fd

--- octavia/controller/worker/tasks/network_tasks.py
+++ octavia/controller/worker/tasks/network_tasks.py
@@ -83,14 +83,12 @@ class CalculateDelta(BaseNetworkTask):
                 if (not listener.default_pool) or (
                         not listener.default_pool.members):
                     continue
-                desired_network_ids.update(
-                    list(
-                        self.network_driver.get_network(
-                            subnet_id=member.subnet_id).id
-                        for member in listener.default_pool.members
-                        if member.subnet_id
-                    )
-                )
+                member_networks = [
+                    self.network_driver.get_subnet(member.subnet_id).network_id
+                    for member in listener.default_pool.members
+                    if member.subnet_id
+                ]
+                desired_network_ids.update(member_networks)
 
             nics = self.network_driver.get_plugged_networks(amphora.compute_id)
             # assume we don't have two nics in the same network
