636165b852ae00007e0d55e657bf9113b20832fe
Jamie Lennox <jamielennox@gmail.com>
2016-07-27 15:24:01 -0500

    Use the from_environ features of oslo_context
    Oslo context provides a from_environ features that loads up a context
    with all the standard auth_token middleware and oslo middleware. Use
    that instead of checking those ourselves.
    
    Change-Id: Id19aa796bf284348667b8e02f91bec4dba9d7b4a

--- octavia/api/v1/hooks.py
+++ octavia/api/v1/hooks.py
@@ -20,12 +20,5 @@ from octavia.common import context
 class ContextHook(hooks.PecanHook):
 
     def on_route(self, state):
-        user_id = state.request.headers.get('X-User-Id')
-        user_id = state.request.headers.get('X-User', user_id)
-        project = state.request.headers.get('X-Tenant-Id')
-        project = state.request.headers.get('X-Tenant', project)
-        project = state.request.headers.get('X-Project-Id', project)
-        project = state.request.headers.get('X-Project', project)
-        auth_token = state.request.headers.get('X-Auth-Token')
-        state.request.context['octavia_context'] = context.Context(
-            user_id=user_id, project_id=project, auth_token=auth_token)
+        context_obj = context.Context.from_environ(state.request.environ)
+        state.request.context['octavia_context'] = context_obj
