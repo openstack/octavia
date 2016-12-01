50c2713c36bd61a807673c92951a792c2d048dec
Ankur Gupta <ankur.gupta@intel.com>
2016-12-01 13:57:18 -0600

    Introduct Test Base classes for V2 API
    Change-Id: Ibc91eaf7fb052194c7ed49d62ff716371917f76d

---
+++ octavia/tests/unit/api/v2/handlers/queue/test_producer.py
@@ -0,0 +1,236 @@
+#    Copyright 2014 Rackspace
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.#    Copyright 2014 Rackspace
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+from oslo_config import cfg
+from oslo_config import fixture as oslo_fixture
+import oslo_messaging as messaging
+
+from octavia.api.v1.handlers.queue import producer
+from octavia.api.v1.types import health_monitor
+from octavia.api.v1.types import l7policy
+from octavia.api.v1.types import l7rule
+from octavia.api.v1.types import listener
+from octavia.api.v1.types import load_balancer
+from octavia.api.v1.types import member
+from octavia.api.v1.types import pool
+from octavia.common import data_models
+from octavia.tests.unit import base
+
+
+class TestProducer(base.TestCase):
+    def setUp(self):
+        super(TestProducer, self).setUp()
+        self.mck_model = mock.Mock()
+        self.mck_model.id = '10'
+        conf = self.useFixture(oslo_fixture.Config(cfg.CONF))
+        conf.config(group="oslo_messaging", topic='OCTAVIA_PROV')
+        mck_target = mock.patch(
+            'octavia.api.v1.handlers.queue.producer.messaging.Target')
+        mck_transport = mock.patch(
+            'octavia.api.v1.handlers.queue.producer.messaging.get_transport')
+        self.mck_client = mock.create_autospec(messaging.RPCClient)
+        mck_client = mock.patch(
+            'octavia.api.v1.handlers.queue.producer.messaging.RPCClient',
+            return_value=self.mck_client)
+        mck_target.start()
+        mck_transport.start()
+        mck_client.start()
+        self.addCleanup(mck_target.stop)
+        self.addCleanup(mck_transport.stop)
+        self.addCleanup(mck_client.stop)
+
+    def test_create_loadbalancer(self):
+        p = producer.LoadBalancerProducer()
+        p.create(self.mck_model)
+        kw = {'load_balancer_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'create_load_balancer', **kw)
+
+    def test_delete_loadbalancer(self):
+        p = producer.LoadBalancerProducer()
+        p.delete(self.mck_model, False)
+        kw = {'load_balancer_id': self.mck_model.id,
+              'cascade': False}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'delete_load_balancer', **kw)
+
+    def test_update_loadbalancer(self):
+        p = producer.LoadBalancerProducer()
+        lb = data_models.LoadBalancer(id=10)
+        lb_updates = load_balancer.LoadBalancerPUT(enabled=False)
+        p.update(lb, lb_updates)
+        kw = {'load_balancer_id': lb.id,
+              'load_balancer_updates': lb_updates.to_dict(render_unsets=False)}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'update_load_balancer', **kw)
+
+    def test_create_listener(self):
+        p = producer.ListenerProducer()
+        p.create(self.mck_model)
+        kw = {'listener_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'create_listener', **kw)
+
+    def test_delete_listener(self):
+        p = producer.ListenerProducer()
+        p.delete(self.mck_model)
+        kw = {'listener_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'delete_listener', **kw)
+
+    def test_update_listener(self):
+        p = producer.ListenerProducer()
+        listener_model = data_models.LoadBalancer(id=10)
+        listener_updates = listener.ListenerPUT(enabled=False)
+        p.update(listener_model, listener_updates)
+        kw = {'listener_id': listener_model.id,
+              'listener_updates': listener_updates.to_dict(
+                  render_unsets=False)}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'update_listener', **kw)
+
+    def test_create_pool(self):
+        p = producer.PoolProducer()
+        p.create(self.mck_model)
+        kw = {'pool_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'create_pool', **kw)
+
+    def test_delete_pool(self):
+        p = producer.PoolProducer()
+        p.delete(self.mck_model)
+        kw = {'pool_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'delete_pool', **kw)
+
+    def test_update_pool(self):
+        p = producer.PoolProducer()
+        pool_model = data_models.Pool(id=10)
+        pool_updates = pool.PoolPUT(enabled=False)
+        p.update(pool_model, pool_updates)
+        kw = {'pool_id': pool_model.id,
+              'pool_updates': pool_updates.to_dict(render_unsets=False)}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'update_pool', **kw)
+
+    def test_create_healthmonitor(self):
+        p = producer.HealthMonitorProducer()
+        p.create(self.mck_model)
+        kw = {'health_monitor_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'create_health_monitor', **kw)
+
+    def test_delete_healthmonitor(self):
+        p = producer.HealthMonitorProducer()
+        p.delete(self.mck_model)
+        kw = {'health_monitor_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'delete_health_monitor', **kw)
+
+    def test_update_healthmonitor(self):
+        p = producer.HealthMonitorProducer()
+        hm = data_models.HealthMonitor(pool_id=10)
+        hm_updates = health_monitor.HealthMonitorPUT(enabled=False)
+        p.update(hm, hm_updates)
+        kw = {'pool_id': hm.pool_id,
+              'health_monitor_updates': hm_updates.to_dict(
+                  render_unsets=False)}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'update_health_monitor', **kw)
+
+    def test_create_member(self):
+        p = producer.MemberProducer()
+        p.create(self.mck_model)
+        kw = {'member_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'create_member', **kw)
+
+    def test_delete_member(self):
+        p = producer.MemberProducer()
+        p.delete(self.mck_model)
+        kw = {'member_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'delete_member', **kw)
+
+    def test_update_member(self):
+        p = producer.MemberProducer()
+        member_model = data_models.Member(id=10)
+        member_updates = member.MemberPUT(enabled=False)
+        p.update(member_model, member_updates)
+        kw = {'member_id': member_model.id,
+              'member_updates': member_updates.to_dict(render_unsets=False)}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'update_member', **kw)
+
+    def test_create_l7policy(self):
+        p = producer.L7PolicyProducer()
+        p.create(self.mck_model)
+        kw = {'l7policy_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'create_l7policy', **kw)
+
+    def test_delete_l7policy(self):
+        p = producer.L7PolicyProducer()
+        p.delete(self.mck_model)
+        kw = {'l7policy_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'delete_l7policy', **kw)
+
+    def test_update_l7policy(self):
+        p = producer.L7PolicyProducer()
+        l7policy_model = data_models.L7Policy(id=10)
+        l7policy_updates = l7policy.L7PolicyPUT(enabled=False)
+        p.update(l7policy_model, l7policy_updates)
+        kw = {'l7policy_id': l7policy_model.id,
+              'l7policy_updates': l7policy_updates.to_dict(
+                  render_unsets=False)}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'update_l7policy', **kw)
+
+    def test_create_l7rule(self):
+        p = producer.L7RuleProducer()
+        p.create(self.mck_model)
+        kw = {'l7rule_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'create_l7rule', **kw)
+
+    def test_delete_l7rule(self):
+        p = producer.L7RuleProducer()
+        p.delete(self.mck_model)
+        kw = {'l7rule_id': self.mck_model.id}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'delete_l7rule', **kw)
+
+    def test_update_l7rule(self):
+        p = producer.L7RuleProducer()
+        l7rule_model = data_models.L7Rule(id=10)
+        l7rule_updates = l7rule.L7RulePUT(enabled=False)
+        p.update(l7rule_model, l7rule_updates)
+        kw = {'l7rule_id': l7rule_model.id,
+              'l7rule_updates': l7rule_updates.to_dict(render_unsets=False)}
+        self.mck_client.cast.assert_called_once_with(
+            {}, 'update_l7rule', **kw)
---
+++ octavia/tests/unit/api/v2/types/base.py
@@ -0,0 +1,225 @@
+#    Copyright 2014 Rackspace
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_utils import uuidutils
+from wsme import exc
+from wsme.rest import json as wsme_json
+
+from octavia.api.v1.types import base as base_type
+from octavia.common import constants
+from octavia.tests.unit import base
+
+
+def build_body(mandatory_fields, extra_attributes):
+    body = {}
+    for key in mandatory_fields:
+        body[key] = mandatory_fields[key]
+    for key in extra_attributes:
+        body[key] = extra_attributes[key]
+    return body
+
+
+class BaseTypesTest(base.TestCase):
+    _type = base_type.BaseType
+    _mandatory_fields = {}
+
+
+class BaseTestUuid(base.TestCase):
+
+    def assert_uuid_attr(self, attr):
+        kwargs = {attr: uuidutils.generate_uuid()}
+        self._type(**kwargs)
+
+    def assert_uuid_attr_fail_with_integer(self, attr):
+        kwargs = {attr: 1}
+        self.assertRaises(exc.InvalidInput, self._type, **kwargs)
+
+    def assert_uuid_attr_fail_with_short_str(self, attr):
+        kwargs = {attr: '12345'}
+        self.assertRaises(exc.InvalidInput, self._type, **kwargs)
+
+    def assert_uuid_attr_fail_with_shorter_than_uuid(self, attr):
+        kwargs = {attr: uuidutils.generate_uuid()[1:]}
+        self.assertRaises(exc.InvalidInput, self._type, **kwargs)
+
+    def assert_uuid_attr_fail_with_longer_than_uuid(self, attr):
+        kwargs = {attr: uuidutils.generate_uuid() + "0"}
+        self.assertRaises(exc.InvalidInput, self._type, **kwargs)
+
+
+class BaseTestString(base.TestCase):
+
+    def _default_min_max_lengths(self, min_length=None, max_length=None):
+        if max_length is None:
+            if min_length is None:
+                max_length = 255
+                min_length = 2
+            else:
+                max_length = min_length + 1
+        else:
+            if min_length is None:
+                min_length = max_length - 1
+        return min_length, max_length
+
+    def assert_string_attr(self, attr, min_length=None, max_length=None):
+        min_length, max_length = self._default_min_max_lengths(min_length,
+                                                               max_length)
+        string_val = 'a' * (max_length - 1)
+        kwargs = {attr: string_val}
+        self._type(**kwargs)
+
+    def assert_string_attr_min_length(self, attr, min_length):
+        min_length, max_length = self._default_min_max_lengths(min_length)
+        string_val = 'a' * (min_length - 1)
+        kwargs = {attr: string_val}
+        # No point in testing if min_length is <= 0
+        if min_length > 0:
+            self.assertRaises(exc.InvalidInput, self._type, **kwargs)
+
+    def assert_string_attr_max_length(self, attr, max_length=None):
+        min_length, max_length = self._default_min_max_lengths(max_length)
+        string_val = 'a' * (max_length + 1)
+        kwargs = {attr: string_val}
+        self.assertRaises(exc.InvalidInput, self._type, **kwargs)
+
+
+class BaseTestBool(base.TestCase):
+
+    def assert_bool_attr(self, attr):
+        kwargs = {attr: True}
+        self.assertIsNotNone(self._type(**kwargs))
+        kwargs = {attr: False}
+        self.assertIsNotNone(self._type(**kwargs))
+
+    def assert_bool_attr_non_bool(self, attr):
+        kwargs = {attr: 'test'}
+        self.assertRaises(exc.InvalidInput, self._type, **kwargs)
+
+
+class TestIdMixin(BaseTestUuid):
+    id_attr = 'id'
+
+    def test_id(self):
+        self.assert_uuid_attr(self.id_attr)
+        self.assert_uuid_attr_fail_with_integer(self.id_attr)
+        self.assert_uuid_attr_fail_with_short_str(self.id_attr)
+        self.assert_uuid_attr_fail_with_shorter_than_uuid(self.id_attr)
+        self.assert_uuid_attr_fail_with_longer_than_uuid(self.id_attr)
+
+    def test_id_readonly(self):
+        body = build_body(self._mandatory_fields,
+                          {self.id_attr: uuidutils.generate_uuid()})
+        self.assertRaises(exc.InvalidInput, wsme_json.fromjson,
+                          self._type, body)
+
+
+class TestProjectIdMixin(BaseTestUuid):
+    project_id_attr = 'project_id'
+
+    def test_project_id(self):
+        self.assert_uuid_attr(self.project_id_attr)
+        self.assert_uuid_attr_fail_with_integer(self.project_id_attr)
+        self.assert_uuid_attr_fail_with_short_str(self.project_id_attr)
+        self.assert_uuid_attr_fail_with_shorter_than_uuid(self.project_id_attr)
+        self.assert_uuid_attr_fail_with_longer_than_uuid(self.project_id_attr)
+
+    def test_project_id_readonly(self):
+        body = build_body(self._mandatory_fields,
+                          {self.project_id_attr: uuidutils.generate_uuid()})
+        self.assertRaises(exc.InvalidInput, wsme_json.fromjson,
+                          self._type, body)
+
+
+class TestNameMixin(BaseTestString):
+    name_attr = 'name'
+
+    def test_name(self):
+        self.assert_string_attr(self.name_attr, min_length=0, max_length=255)
+        self.assert_string_attr_min_length(self.name_attr, 0)
+        self.assert_string_attr_max_length(self.name_attr, 255)
+
+    def test_editable_name(self):
+        name = "Name"
+        body = build_body(self._mandatory_fields, {self.name_attr: name})
+        type_instance = wsme_json.fromjson(self._type, body)
+        self.assertEqual(name, type_instance.name)
+
+
+class TestDescriptionMixin(BaseTestString):
+    description_attr = 'description'
+
+    def test_description(self):
+        self.assert_string_attr(self.description_attr, min_length=0,
+                                max_length=255)
+        self.assert_string_attr_min_length(self.description_attr, 0)
+        self.assert_string_attr_max_length(self.description_attr, 255)
+
+    def test_editable_description(self):
+        description = "Description"
+        body = build_body(self._mandatory_fields,
+                          {self.description_attr: description})
+        type_instance = wsme_json.fromjson(self._type, body)
+        self.assertEqual(description, type_instance.description)
+
+
+class TestEnabledMixin(BaseTestBool):
+    enabled_attr = 'enabled'
+
+    def test_enabled(self):
+        self.assert_bool_attr(self.enabled_attr)
+        self.assert_bool_attr_non_bool(self.enabled_attr)
+
+    def test_default_enabled_true(self):
+        body = build_body(self._mandatory_fields, {})
+        type_instance = wsme_json.fromjson(self._type, body)
+        self.assertTrue(type_instance.enabled)
+
+    def test_editable_enabled(self):
+        body = build_body(self._mandatory_fields, {"enabled": False})
+        type_instance = wsme_json.fromjson(self._type, body)
+        self.assertFalse(type_instance.enabled)
+
+
+class TestProvisioningStatusMixin(BaseTestString):
+    provisioning_attr = 'provisioning_status'
+
+    def test_provisioning_status(self):
+        self.assert_string_attr(self.provisioning_attr, min_length=0,
+                                max_length=16)
+        self.assert_string_attr_min_length(self.provisioning_attr, 0)
+        self.assert_string_attr_max_length(self.provisioning_attr, 16)
+
+    def test_provisioning_status_readonly(self):
+        status = constants.ACTIVE
+        body = build_body(self._mandatory_fields,
+                          {self.provisioning_attr: status})
+        self.assertRaises(exc.InvalidInput, wsme_json.fromjson,
+                          self._type, body)
+
+
+class TestOperatingStatusMixin(BaseTestString):
+    operating_attr = 'operating_status'
+
+    def test_operating_status(self):
+        self.assert_string_attr(self.operating_attr, min_length=0,
+                                max_length=16)
+        self.assert_string_attr_min_length(self.operating_attr, 0)
+        self.assert_string_attr_max_length(self.operating_attr, 16)
+
+    def test_operating_status_readonly(self):
+        status = constants.ONLINE
+        body = build_body(self._mandatory_fields,
+                          {self.operating_attr: status})
+        self.assertRaises(exc.InvalidInput, wsme_json.fromjson,
+                          self._type, body)
