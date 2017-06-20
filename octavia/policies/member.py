7fa12cee40d75dc9f055bdd207c5fd2929e3616c
Michael Johnson <johnsomor@gmail.com>
2017-06-20 13:48:01 -0700

    Add RBAC enforcement to members v2 API
    This patch adds policies and enforcement to the Octavia v2 API for members.
    
    Change-Id: I8f369e8ad6fa1cf3ee6485f0be95b243b7ade20e
    Partial-Bug: #1690481

---
+++ octavia/policies/member.py
@@ -0,0 +1,60 @@
+# Copyright 2017 Rackspace, US Inc.
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from octavia.common import constants
+from oslo_policy import policy
+
+rules = [
+    policy.DocumentedRuleDefault(
+        '{rbac_obj}{action}'.format(rbac_obj=constants.RBAC_MEMBER,
+                                    action='get_all'),
+        constants.RULE_API_READ,
+        "List Members of a Pool",
+        [{'method': 'GET', 'path': '/v2.0/lbaas/pools/{pool_id}/members'}]
+    ),
+    policy.DocumentedRuleDefault(
+        '{rbac_obj}{action}'.format(rbac_obj=constants.RBAC_MEMBER,
+                                    action='post'),
+        constants.RULE_API_WRITE,
+        "Create a Member",
+        [{'method': 'POST', 'path': '/v2.0/lbaas/pools/{pool_id}/members'}]
+    ),
+    policy.DocumentedRuleDefault(
+        '{rbac_obj}{action}'.format(rbac_obj=constants.RBAC_MEMBER,
+                                    action='get_one'),
+        constants.RULE_API_READ,
+        "Show Member details",
+        [{'method': 'GET',
+          'path': '/v2.0/lbaas/pools/{pool_id}/members/{member_id}'}]
+    ),
+    policy.DocumentedRuleDefault(
+        '{rbac_obj}{action}'.format(rbac_obj=constants.RBAC_MEMBER,
+                                    action='put'),
+        constants.RULE_API_WRITE,
+        "Update a Member",
+        [{'method': 'PUT',
+          'path': '/v2.0/lbaas/pools/{pool_id}/members/{member_id}'}]
+    ),
+    policy.DocumentedRuleDefault(
+        '{rbac_obj}{action}'.format(rbac_obj=constants.RBAC_MEMBER,
+                                    action='delete'),
+        constants.RULE_API_WRITE,
+        "Remove a Member",
+        [{'method': 'DELETE',
+          'path': '/v2.0/lbaas/pools/{pool_id}/members/{member_id}'}]
+    ),
+]
+
+
+def list_rules():
+    return rules
