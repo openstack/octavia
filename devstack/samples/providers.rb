a7256e15edcf9b6360896a92f3e02d2cc383e0a5
ckji <ckji@fiberhome.com>
2017-06-28 11:29:49 +0800

    Change default vagrant box to xenial64(16.04)
    Because current devstack don't support ubuntu 14.04. Change default
    vagrant box from trusty64 to xenial64.
    
    See:
    https://docs.openstack.org/developer/devstack/#install-linux
    
    Change-Id: Id49167e6d60babcc6145c0f4cbd5b15231d12c88

--- devstack/samples/providers.rb
+++ devstack/samples/providers.rb
@@ -5,14 +5,14 @@ VM_CPUS = ENV['VM_CPUS'] || "1"
 def configure_providers(vm)
 
   vm.provider "virtualbox" do |vb, config|
-     config.vm.box = "ubuntu/trusty64"
+     config.vm.box = "ubuntu/xenial64"
      vb.gui = true
      vb.memory = VM_MEMORY
      vb.cpus = VM_CPUS
   end
 
   vm.provider "libvirt" do |lb, config|
-     config.vm.box = "celebdor/trusty64"
+     config.vm.box = "celebdor/xenial64"
      config.vm.synced_folder './', '/vagrant', type: 'rsync'
      lb.nested = true
      lb.memory = VM_MEMORY
