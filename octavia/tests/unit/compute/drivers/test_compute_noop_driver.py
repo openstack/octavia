9e4c30156f9b7752329fc1790c82c0a5d9270fbb
Adam Harwell <flux.adam@gmail.com>
2017-12-04 12:07:15 -0800

    Update compute noop driver/tests (many were disabled/useless)
    Change-Id: I96f9c963bb4c057be8951aee8aea89eb2e88ce14

--- octavia/tests/unit/compute/drivers/test_compute_noop_driver.py
+++ octavia/tests/unit/compute/drivers/test_compute_noop_driver.py
@@ -22,6 +22,9 @@ class TestNoopComputeDriver(base.TestCase):
     FAKE_UUID_1 = uuidutils.generate_uuid()
     FAKE_UUID_2 = uuidutils.generate_uuid()
     FAKE_UUID_3 = uuidutils.generate_uuid()
+    FAKE_UUID_4 = uuidutils.generate_uuid()
+    FAKE_UUID_5 = uuidutils.generate_uuid()
+    FAKE_UUID_6 = uuidutils.generate_uuid()
 
     def setUp(self):
         super(TestNoopComputeDriver, self).setUp()
@@ -29,41 +32,43 @@ class TestNoopComputeDriver(base.TestCase):
 
         self.name = "amphora_name"
         self.amphora_flavor = "m1.tiny"
-        self.image_id = self.FAKE_UUID_2
+        self.image_id = self.FAKE_UUID_1
         self.image_tag = "faketag"
+        self.image_owner = self.FAKE_UUID_2
         self.key_name = "key_name"
-        self.sec_groups = "default"
-        self.network_ids = self.FAKE_UUID_3
-        self.confdrivefiles = "config_driver_files"
+        self.sec_groups = ["default"]
+        self.network_ids = [self.FAKE_UUID_3]
+        self.confdrivefiles = ["config_driver_file1"]
         self.user_data = "user_data"
-        self.amphora_id = self.FAKE_UUID_1
-        self.loadbalancer_id = self.FAKE_UUID_1
+        self.amphora_id = self.FAKE_UUID_4
+        self.loadbalancer_id = self.FAKE_UUID_5
         self.server_group_policy = 'anti-affinity'
-        self.server_group_id = self.FAKE_UUID_1
+        self.server_group_name = 'my_server_group'
+        self.server_group_id = self.FAKE_UUID_6
+        self.port_ids = ['port-id-1']
 
-    def build(self):
+    def test_build(self):
         self.driver.build(self.name, self.amphora_flavor,
-                          self.image_id, self.image_tag,
+                          self.image_id, self.image_tag, self.image_owner,
                           self.key_name, self.sec_groups, self.network_ids,
-                          self.confdrivefiles, self.user_data,
+                          self.confdrivefiles, self.user_data, self.port_ids,
                           self.server_group_id)
 
         self.assertEqual((self.name, self.amphora_flavor,
-                          self.image_id, self.image_tag,
+                          self.image_id, self.image_tag, self.image_owner,
                           self.key_name, self.sec_groups, self.network_ids,
-                          self.config_drive_files, self.user_data,
+                          self.confdrivefiles, self.user_data, self.port_ids,
                           self.server_group_id, 'build'),
-                         self.driver.driver.computeconfig[(self.name,
-                                                           self.amphora_flavor,
-                                                           self.image_id,
-                                                           self.image_tag,
-                                                           self.key_name,
-                                                           self.sec_groups,
-                                                           self.network_ids,
-                                                           self.confdrivefiles,
-                                                           self.user_data,
-                                                           self.server_group_id
-                                                           )])
+                         self.driver.driver.computeconfig[(
+                             self.name,
+                             self.amphora_flavor,
+                             self.image_id,
+                             self.image_tag,
+                             self.image_owner,
+                             self.key_name,
+                             self.user_data,
+                             self.server_group_id
+                         )])
 
     def test_delete(self):
         self.driver.delete(self.amphora_id)
@@ -71,15 +76,28 @@ class TestNoopComputeDriver(base.TestCase):
                          self.driver.driver.computeconfig[
                              self.amphora_id])
 
-    def status(self):
+    def test_status(self):
         self.driver.status(self.amphora_id)
+        self.assertEqual((self.amphora_id, 'status'),
+                         self.driver.driver.computeconfig[
+                             self.amphora_id])
 
-    def get_amphora(self):
+    def test_get_amphora(self):
         self.driver.get_amphora(self.amphora_id)
+        self.assertEqual((self.amphora_id, 'get_amphora'),
+                         self.driver.driver.computeconfig[
+                             self.amphora_id])
 
     def test_create_server_group(self):
-        self.driver.create_server_group(self.loadbalancer_id,
+        self.driver.create_server_group(self.server_group_name,
                                         self.server_group_policy)
+        self.assertEqual((self.server_group_name, self.server_group_policy,
+                          'create'),
+                         self.driver.driver.computeconfig[
+                             self.server_group_name, self.server_group_policy])
 
     def test_delete_server_group(self):
         self.driver.delete_server_group(self.server_group_id)
+        self.assertEqual((self.server_group_id, 'delete'),
+                         self.driver.driver.computeconfig[
+                             self.server_group_id])
