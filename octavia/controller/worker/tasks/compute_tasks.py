a4b17833378f8642b5cef1a0624adbb806bf487f
Michael Johnson <johnsomor@gmail.com>
2018-07-30 16:48:07 -0700

    Delete amphora regardless of status
    Now that we are actively using the amphora status of ERROR we need to make
    sure that LB delete will delete amphora in the status of ERROR.
    
    Change-Id: I808ef7711fde309878b0df305be81d58319b70b7

--- octavia/controller/worker/tasks/compute_tasks.py
+++ octavia/controller/worker/tasks/compute_tasks.py
@@ -17,7 +17,6 @@ import time
 
 from oslo_config import cfg
 from oslo_log import log as logging
-import six
 from stevedore import driver as stevedore_driver
 from taskflow import task
 from taskflow.types import failure
@@ -151,10 +150,8 @@ class DeleteAmphoraeOnLoadBalancer(BaseComputeTask):
     """
 
     def execute(self, loadbalancer):
-        for amp in six.moves.filter(
-            lambda amp: amp.status == constants.AMPHORA_ALLOCATED,
-                loadbalancer.amphorae):
-
+        for amp in loadbalancer.amphorae:
+            # The compute driver will already handle NotFound
             try:
                 self.compute.delete(amp.compute_id)
             except Exception:
