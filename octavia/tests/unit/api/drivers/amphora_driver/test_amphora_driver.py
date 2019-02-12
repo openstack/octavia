0a6ef71c2ea48d8094b2c1554247c7c74d8031a1
Erik Olof Gunnar Andersson <eandersson@blizzard.com>
2019-02-12 07:13:26 -0800

    Fix oslo messaging connection leakage
    Conflicts:
        octavia/api/drivers/amphora_driver/driver.py
        octavia/api/v2/controllers/amphora.py
    
    Story: 2004993
    Task: 29464
    
    Change-Id: I8e9cca7c0a7eb82b2a029a3ead2486dd1742b65f
    (cherry picked from commit ad7e627185c62e4fd0f0b29228bf202f9578a858)

--- octavia/tests/unit/api/drivers/amphora_driver/test_amphora_driver.py
+++ octavia/tests/unit/api/drivers/amphora_driver/test_amphora_driver.py
@@ -11,7 +11,6 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
 import mock
 
 from octavia.api.drivers.amphora_driver import driver
@@ -23,7 +22,7 @@ from octavia.tests.unit.api.drivers import sample_data_models
 from octavia.tests.unit import base
 
 
-class TestAmphoraDriver(base.TestCase):
+class TestAmphoraDriver(base.TestRpc):
     def setUp(self):
         super(TestAmphoraDriver, self).setUp()
         self.amp_driver = driver.AmphoraProviderDriver()
--- octavia/tests/unit/controller/queue/test_consumer.py
+++ octavia/tests/unit/controller/queue/test_consumer.py
@@ -16,18 +16,13 @@ import mock
 from oslo_config import cfg
 from oslo_config import fixture as oslo_fixture
 import oslo_messaging as messaging
-from oslo_messaging.rpc import dispatcher
 
 from octavia.controller.queue import consumer
 from octavia.controller.queue import endpoint
 from octavia.tests.unit import base
 
 
-@mock.patch.object(messaging, 'get_rpc_transport')
-@mock.patch.object(messaging, 'Target')
-@mock.patch.object(endpoint, 'Endpoint')
-@mock.patch.object(messaging, 'get_rpc_server')
-class TestConsumer(base.TestCase):
+class TestConsumer(base.TestRpc):
 
     def setUp(self):
         super(TestConsumer, self).setUp()
@@ -36,10 +31,10 @@ class TestConsumer(base.TestCase):
         conf.config(host='test-hostname')
         self.conf = conf.conf
 
-    def test_consumer_run(self, mock_rpc_server, mock_endpoint, mock_target,
-                          mock_get_transport):
-        mock_get_transport_rv = mock.Mock()
-        mock_get_transport.return_value = mock_get_transport_rv
+    @mock.patch.object(messaging, 'Target')
+    @mock.patch.object(endpoint, 'Endpoint')
+    @mock.patch.object(messaging, 'get_rpc_server')
+    def test_consumer_run(self, mock_rpc_server, mock_endpoint, mock_target):
         mock_rpc_server_rv = mock.Mock()
         mock_rpc_server.return_value = mock_rpc_server_rv
         mock_endpoint_rv = mock.Mock()
@@ -49,20 +44,13 @@ class TestConsumer(base.TestCase):
 
         consumer.ConsumerService(1, self.conf).run()
 
-        mock_get_transport.assert_called_once_with(cfg.CONF)
         mock_target.assert_called_once_with(topic='foo_topic',
                                             server='test-hostname',
                                             fanout=False)
         mock_endpoint.assert_called_once_with()
-        access_policy = dispatcher.DefaultRPCAccessPolicy
-        mock_rpc_server.assert_called_once_with(mock_get_transport_rv,
-                                                mock_target_rv,
-                                                [mock_endpoint_rv],
-                                                executor='threading',
-                                                access_policy=access_policy)
 
-    def test_consumer_terminate(self, mock_rpc_server, mock_endpoint,
-                                mock_target, mock_get_transport):
+    @mock.patch.object(messaging, 'get_rpc_server')
+    def test_consumer_terminate(self, mock_rpc_server):
         mock_rpc_server_rv = mock.Mock()
         mock_rpc_server.return_value = mock_rpc_server_rv
 
@@ -72,8 +60,8 @@ class TestConsumer(base.TestCase):
         mock_rpc_server_rv.stop.assert_called_once_with()
         self.assertFalse(mock_rpc_server_rv.wait.called)
 
-    def test_consumer_graceful_terminate(self, mock_rpc_server, mock_endpoint,
-                                         mock_target, mock_get_transport):
+    @mock.patch.object(messaging, 'get_rpc_server')
+    def test_consumer_graceful_terminate(self, mock_rpc_server):
         mock_rpc_server_rv = mock.Mock()
         mock_rpc_server.return_value = mock_rpc_server_rv
 
