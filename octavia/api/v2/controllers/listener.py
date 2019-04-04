8e3e8b1ff6263fc3301b93935f3087ef90320f26
Michael Johnson <johnsomor@gmail.com>
2019-04-04 14:29:55 -0700

    Fix listener API handling of None/null updates
    The current listener API does not properly handle clearing/reseting
    values on update. Some integer only fields, such as connection-limit,
    will accept null, but will store the value as "None". These will
    will cause failures updating the amphora configuration.
    
    This patch corrects this to appropriately handle None/null updates
    to the listener parameters.
    
    Change-Id: I41c9bedd8a3452513af3d409fbacd65ea287f02a
    Story: 2005374
    Task: 30352
    (cherry picked from commit 930a3236bf7fd341b32ba41b2c99cd3ad832d709)

--- octavia/api/v2/controllers/listener.py
+++ octavia/api/v2/controllers/listener.py
@@ -415,6 +415,34 @@ class ListenersController(base.BaseController):
         if ca_ref or crl_ref:
             self._validate_client_ca_and_crl_refs(ca_ref, crl_ref)
 
+    def _set_default_on_none(self, listener):
+        """Reset settings to their default values if None/null was passed in
+
+        A None/null value can be passed in to clear a value. PUT values
+        that were not provided by the user have a type of wtypes.UnsetType.
+        If the user is attempting to clear values, they should either
+        be set to None (for example in the name field) or they should be
+        reset to their default values.
+        This method is intended to handle those values that need to be set
+        back to a default value.
+        """
+        if listener.connection_limit is None:
+            listener.connection_limit = constants.DEFAULT_CONNECTION_LIMIT
+        if listener.timeout_client_data is None:
+            listener.timeout_client_data = (
+                CONF.haproxy_amphora.timeout_client_data)
+        if listener.timeout_member_connect is None:
+            listener.timeout_member_connect = (
+                CONF.haproxy_amphora.timeout_member_connect)
+        if listener.timeout_member_data is None:
+            listener.timeout_member_data = (
+                CONF.haproxy_amphora.timeout_member_data)
+        if listener.timeout_tcp_inspect is None:
+            listener.timeout_tcp_inspect = (
+                CONF.haproxy_amphora.timeout_tcp_inspect)
+        if listener.client_authentication is None:
+            listener.client_authentication = constants.CLIENT_AUTH_NONE
+
     @wsme_pecan.wsexpose(listener_types.ListenerRootResponse, wtypes.text,
                          body=listener_types.ListenerRootPUT, status_code=200)
     def put(self, id, listener_):
@@ -431,6 +459,8 @@ class ListenersController(base.BaseController):
         self._auth_validate_action(context, project_id, constants.RBAC_PUT)
 
         self._validate_listener_PUT(listener, db_listener)
+
+        self._set_default_on_none(listener)
 
         if listener.default_pool_id:
             self._validate_pool(context.session, load_balancer_id,
