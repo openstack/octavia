d2bef9a47c6497b479d91471fbaec60d993e86ac
Michael Johnson <johnsomor@gmail.com>
2020-06-04 10:45:35 -0700

    Fix batch member create for v1 amphora driver
    A previous patch[1] missed batch_member_update when adding database
    repository "get" method retries for new object creation actions.
    This patch fixes batch member create to retry the database get call
    when new members are being created via batch member update.
    This issue only impacts the v1 amphora driver as the v2 driver
    does not need to get these objects from the database.
    
    Story: 2007581
    Task: 39503
    
    [1] https://github.com/openstack/octavia/commit/48e85569f7e43e3dd5f09fd1ef4cb165526a92cd
    
    Change-Id: Ia3476ab7b24dc3fd6e29ff2abe6eb6bacd9908ed

--- octavia/tests/unit/controller/worker/test_controller_worker.py
+++ octavia/tests/unit/controller/worker/test_controller_worker.py
@@ -824,7 +824,8 @@ class TestControllerWorker(base.TestCase):
                                   mock_amp_repo_get):
 
         _flow_mock.reset_mock()
-
+        mock_member_repo_get.side_effect = [None, _member_mock,
+                                            _member_mock, _member_mock]
         cw = controller_worker.ControllerWorker()
         cw.batch_update_members([9], [11], [MEMBER_UPDATE_DICT])
 
@@ -837,6 +838,7 @@ class TestControllerWorker(base.TestCase):
                                         constants.POOL: _pool_mock}))
 
         _flow_mock.run.assert_called_once_with()
+        self.assertEqual(4, mock_member_repo_get.call_count)
 
     @mock.patch('octavia.controller.worker.flows.'
                 'pool_flows.PoolFlows.get_create_pool_flow',
