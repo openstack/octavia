a8266a9e4f89c0c2120994e3d6b98c1ffdac4877
Xing Zhang <angeiv.zhang@gmail.com>
2021-03-02 12:55:35 +0800

    Fix weighted round-robin about UDP listener
    'lb_algo rr' in keepalived won't work correctly with weight
    for UDP listeners, it should be 'lb_algo wrr'.
    
    'wrr' is superset of 'rr' algorithm, as round-robin is
    a specical instance of the weighted round-robin scheduling,
    in which all the weights are equal. [1]
    
    Algorithm in HAProxy is set to 'roundrobin', which also support
    weights, but in keepalived must be set to 'wrr' to work with
    the weighted round-robin scheduling, as it is different to 'rr'.
    
    [1] https://www.keepalived.org/doc/scheduling_algorithms.html
    
    Story 2008462
    Task 41491
    
    Conflicts:
    	octavia/tests/unit/common/jinja/lvs/test_jinja_cfg.py
    	octavia/tests/unit/common/sample_configs/sample_configs_combined.py
    
    NOTE(Xing Zhang): The first file does not exist in stable/victoria
    due to unit test structure was fixed, file renamed from test_lvs_jinja_cfg.py,
    (patch I6d84047b3481a2bf6bf9bd17d482fb504dbc752b) was introduced in stable/wallaby.
    The second file has conflict due to the SCTP is supported by the amphora provider
    in the Wallaby release. (patch I30997ae6cc6b8ec724f0e9dcfdfe49356b320ff4)
    
    This backport removes SCTP in commit message and releasenote due to SCTP
    was introduced in the Wallaby release.
    
    Change-Id: Ic63929d8864e5285baf70dd85e6362988bf2863f
    (cherry picked from commit 5352a10f62f1a4763aeee81e2e638a9896b6ce79)
    (cherry picked from commit d9603b3d21723fce3b7380b14d537d847088246c)
    (cherry picked from commit c7640b90ad549566e23a97fbcf2f2ad387bed7d4)

--- octavia/tests/unit/common/sample_configs/sample_configs_combined.py
+++ octavia/tests/unit/common/sample_configs/sample_configs_combined.py
@@ -468,7 +468,7 @@ RET_UDP_POOL = {
     'id': 'sample_pool_id_1',
     'enabled': True,
     'health_monitor': UDP_HEALTH_MONITOR_NO_SCRIPT,
-    'lb_algorithm': 'rr',
+    'lb_algorithm': 'wrr',
     'members': [UDP_MEMBER_1, UDP_MEMBER_2],
     'protocol': 'udp',
     'session_persistence': UDP_SOURCE_IP_BODY
@@ -480,7 +480,7 @@ RET_UDP_LISTENER = {
         'id': 'sample_pool_id_1',
         'enabled': True,
         'health_monitor': RET_UDP_HEALTH_MONITOR,
-        'lb_algorithm': 'rr',
+        'lb_algorithm': 'wrr',
         'members': [UDP_MEMBER_1, UDP_MEMBER_2],
         'protocol': 'udp',
         'session_persistence': UDP_SOURCE_IP_BODY
