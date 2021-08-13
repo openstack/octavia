07380006f8ca9ff718cd3e7f941ce7c9ba5ae28e
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2021-08-13 11:54:59 +0530

    Fix duplicate SG creation for listener peer port
    In cases where the listener protcol port is same as the peer port
    and allowed_cidr set to 0.0.0.0/0 explicitly, the listener is not
    provisioned due to duplicate security group creation for peer port
    with None as remote_ip_prefix. Neutron SG defaults remote_ip_prefix
    to 0.0.0.0/0 if not specified or None and hence the error SG rule
    already exists.
    
    Remove the duplicate entry from the updated_ports.
    
    Story: #2009117
    Change-Id: I9dbdb71e9b94bbcc75766a8687a996d5358f3381
    (cherry picked from commit 151a94321074b1eda53d1a582fdf26942db85afd)

--- octavia/network/drivers/neutron/allowed_address_pairs.py
+++ octavia/network/drivers/neutron/allowed_address_pairs.py
@@ -153,6 +153,7 @@ class AllowedAddressPairsDriver(neutron_base.BaseNeutronDriver):
             security_group_id=sec_grp_id)
 
         updated_ports = []
+        listener_peer_ports = []
         for l in load_balancer.listeners:
             if (l.provisioning_status in [constants.PENDING_DELETE,
                                           constants.DELETED]):
@@ -170,11 +171,17 @@ class AllowedAddressPairsDriver(neutron_base.BaseNeutronDriver):
                 port = (l.protocol_port, protocol, None)
                 updated_ports.append(port)
 
-            # As the peer port will hold the tcp connection for keepalived and
-            # haproxy session synchronization, so here the security group rule
-            # should be just related with tcp protocol only.
-            updated_ports.append(
-                (l.peer_port, constants.PROTOCOL_TCP.lower(), None))
+            listener_peer_ports.append(l.peer_port)
+
+        # As the peer port will hold the tcp connection for keepalived and
+        # haproxy session synchronization, so here the security group rule
+        # should be just related with tcp protocol only. To avoid adding
+        # duplicate rules, peer_port info should be added if updated_ports
+        # does not have the peer_port entry with allowed_cidr 0.0.0.0/0
+        tcp_lower = constants.PROTOCOL_TCP.lower()
+        for peer_port in listener_peer_ports:
+            if (peer_port, tcp_lower, "0.0.0.0/0") not in updated_ports:
+                updated_ports.append((peer_port, tcp_lower, None))
 
         # Just going to use port_range_max for now because we can assume that
         # port_range_max and min will be the same since this driver is
