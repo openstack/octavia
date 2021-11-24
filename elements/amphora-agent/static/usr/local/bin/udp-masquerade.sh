2653851e75544f8453cc7a0bd371956bcb86e99d
Gregory Thiemonge <gthiemon@redhat.com>
2021-11-24 16:55:25 +0100

    Fix nft command line with negative priority values
    When using nft with negative priority values it is recommended to use --
    to prevent the parser to interpret the value as an argument.
    
    Story 2009710
    Task 44065
    
    Conflicts:
    	elements/amphora-agent/static/usr/local/bin/udp-masquerade.sh
    
    Change-Id: Ia6bc6eee3df30bfb3c0acccf902267fd2a4d37f6
    (cherry picked from commit 4791cfe93cdde2cb375033164c3bf51e8c65f4c4)
    (cherry picked from commit b47d5dfcf8bfd0502c08a200056e21efe22d8d75)
    (cherry picked from commit 1efeb8d5a4a2e1c2f7e06b831b2c1d3555c0ab8a)

--- elements/amphora-agent/static/usr/local/bin/udp-masquerade.sh
+++ elements/amphora-agent/static/usr/local/bin/udp-masquerade.sh
@@ -36,18 +36,18 @@ if [ "$1" == "add" ]; then
             nft add table ip octavia-ipv4
             nft add chain ip octavia-ipv4 ip-udp-masq { type nat hook postrouting priority 100\;}
             nft add rule ip octavia-ipv4 ip-udp-masq oifname "$3" meta l4proto udp masquerade
-            nft add chain ip octavia-ipv4 prerouting { type filter hook prerouting priority -300 \; }
+            nft -- add chain ip octavia-ipv4 prerouting { type filter hook prerouting priority -300 \; }
             nft add rule ip octavia-ipv4 prerouting iifname "$3" meta l4proto tcp notrack
-            nft add chain ip octavia-ipv4 output { type filter hook output priority -300 \; }
+            nft -- add chain ip octavia-ipv4 output { type filter hook output priority -300 \; }
             nft add rule ip octavia-ipv4 output oifname "$3" meta l4proto tcp notrack
 
         elif [ "$2" == "ipv6" ]; then
             nft add table ip6 octavia-ipv6
             nft add chain ip6 octavia-ipv6 ip6-udp-masq { type nat hook postrouting priority 100\;}
             nft add rule ip6 octavia-ipv6 ip6-udp-masq oifname "$3" meta l4proto udp masquerade
-            nft add chain ip6 octavia-ipv6 prerouting { type filter hook prerouting priority -300 \; }
+            nft -- add chain ip6 octavia-ipv6 prerouting { type filter hook prerouting priority -300 \; }
             nft add rule ip6 octavia-ipv6 prerouting iifname "$3" meta l4proto tcp notrack
-            nft add chain ip6 octavia-ipv6 output { type filter hook output priority -300 \; }
+            nft -- add chain ip6 octavia-ipv6 output { type filter hook output priority -300 \; }
             nft add rule ip6 octavia-ipv6 output oifname "$3" meta l4proto tcp notrack
         else
             usage
