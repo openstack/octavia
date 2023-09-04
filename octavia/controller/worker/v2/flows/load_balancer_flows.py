488adb29b68074d345f6ac92abb27ae4282847d3
Gregory Thiemonge <gthiemon@redhat.com>
2023-09-04 05:56:25 -0400

    Fix amphorae in ERROR during the failover
    When 2 amps were down, the failover flow created the first one and
    needed to update both amp to configure VRRP, but as the 2nd was missing,
    it was set to ERROR. Then the health-manager could not trigger a
    failover becasue amphorae in ERROR are excluded from the automated
    failover process.
    
    This commit changes the tasks that must be run on both amphorae during a
    failover of one amphora, it doesn't mark the secondary amphora in ERROR
    if it is not reachable.
    
    Closes-Bug: #2033734
    
    Note: stable/2023.1 and older, the patch also includes modifications in
          octavia/controller/worker/v1/
    
    Conflicts:
    	octavia/controller/worker/v2/tasks/amphora_driver_tasks.py
    
    Change-Id: I4bd027346c61b93b537ab53810c2ecb6160b6be2
    (cherry picked from commit 248cf2893ea913a74d870a763802a51586e0f63b)
    (cherry picked from commit 5622431534d3bf9b8b71ad8fdb2d744f94625ec4)
    (cherry picked from commit 10cfd8c7605e7789ec1ae3de511b3a84b8324e04)
    (cherry picked from commit d09065c4f6c08b07930fa2f8ef6794de5417c2c0)

--- octavia/controller/worker/v2/flows/load_balancer_flows.py
+++ octavia/controller/worker/v2/flows/load_balancer_flows.py
@@ -635,6 +635,7 @@ class LoadBalancerFlows(object):
                           constants.AMP_LISTENER_UPDATE),
                     requires=(constants.LOADBALANCER, constants.AMPHORAE,
                               constants.AMPHORAE_STATUS),
+                    rebind={constants.NEW_AMPHORA_ID: constants.AMPHORA_ID},
                     inject={constants.AMPHORA_INDEX: 0,
                             constants.TIMEOUT_DICT: timeout_dict}))
             update_amps_subflow.add(
@@ -643,6 +644,7 @@ class LoadBalancerFlows(object):
                           constants.AMP_LISTENER_UPDATE),
                     requires=(constants.LOADBALANCER, constants.AMPHORAE,
                               constants.AMPHORAE_STATUS),
+                    rebind={constants.NEW_AMPHORA_ID: constants.AMPHORA_ID},
                     inject={constants.AMPHORA_INDEX: 1,
                             constants.TIMEOUT_DICT: timeout_dict}))
 
@@ -667,6 +669,7 @@ class LoadBalancerFlows(object):
                     name=(new_amp_role + '-' +
                           constants.AMPHORA_RELOAD_LISTENER),
                     requires=(constants.LOADBALANCER, constants.AMPHORAE),
+                    rebind={constants.NEW_AMPHORA_ID: constants.AMPHORA_ID},
                     inject={constants.AMPHORA_INDEX: 1,
                             constants.TIMEOUT_DICT: timeout_dict}))
 
