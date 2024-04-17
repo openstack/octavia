d9ddfcbcf0ac81419d53a1f2093a4f6db0ac577c
Tom Weininger <tweining@redhat.com>
2024-04-17 18:31:41 +0200

    Delete Vagrant related files for devstack
    Change-Id: Ic19bc7d2146433f0f7a502e384c689a74fb74bf6

--- devstack/samples/singlenode/webserver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-MYIP=$(/sbin/ifconfig eth0|grep 'inet addr'|awk -F: '{print $2}'| awk '{print $1}');
-OUTPUT_STR="Welcome to $MYIP\r"
-OUTPUT_LEN=${#OUTPUT_STR}
-
-while true; do
-    echo -e "HTTP/1.0 200 OK\r\nContent-Length: ${OUTPUT_LEN}\r\n\r\n${OUTPUT_STR}" | sudo nc -l -p 80
-done
