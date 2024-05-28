0cea8d67a8a678ce200ef2f39550813914a6745a
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-05-28 18:55:36 +0900

    Drop usage of subtransactions=True
    ... because it was removed in SQLAlchemy 2.0[1].
    
    [1] https://docs.sqlalchemy.org/en/20/changelog/migration_20.html#migration-to-2-0-step-five-use-the-future-flag-on-session
    
    Change-Id: Id8e8232c7170cd5328683222b05c8b644cbaf494

--- octavia/controller/worker/amphora_rate_limit.py
+++ octavia/controller/worker/amphora_rate_limit.py
@@ -48,7 +48,7 @@ class AmphoraBuildRateLimit:
     def has_build_slot(self):
         build_rate_limit = CONF.haproxy_amphora.build_rate_limit
         session = db_apis.get_session()
-        with session.begin(subtransactions=True):
+        with session.begin():
             used_build_slots = (self.amp_build_slots_repo
                                 .get_used_build_slots_count(session))
             available_build_slots = build_rate_limit - used_build_slots
@@ -57,7 +57,7 @@ class AmphoraBuildRateLimit:
 
     def has_highest_priority(self, amphora_id):
         session = db_apis.get_session()
-        with session.begin(subtransactions=True):
+        with session.begin():
             highest_priority_build_req = (
                 self.amp_build_req_repo.get_highest_priority_build_req(
                     session))
@@ -67,13 +67,13 @@ class AmphoraBuildRateLimit:
 
     def update_build_status_and_available_build_slots(self, amphora_id):
         session = db_apis.get_session()
-        with session.begin(subtransactions=True):
+        with session.begin():
             self.amp_build_slots_repo.update_count(session, action='increment')
             self.amp_build_req_repo.update_req_status(session, amphora_id)
 
     def remove_from_build_req_queue(self, amphora_id):
         session = db_apis.get_session()
-        with session.begin(subtransactions=True):
+        with session.begin():
             self.amp_build_req_repo.delete(session, amphora_id=amphora_id)
             self.amp_build_slots_repo.update_count(session, action='decrement')
             LOG.debug("Removed request for %s from queue"
@@ -81,7 +81,7 @@ class AmphoraBuildRateLimit:
 
     def remove_all_from_build_req_queue(self):
         session = db_apis.get_session()
-        with session.begin(subtransactions=True):
+        with session.begin():
             self.amp_build_req_repo.delete_all(session)
             self.amp_build_slots_repo.update_count(session, action='reset')
             LOG.debug("Removed all the build requests and "
