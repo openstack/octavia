d3c5b615cfe02e72577a0b22d05641c26b2110a4
Takashi Kajinami <kajinamit@oss.nttdata.com>
2026-05-31 22:37:59 +0900

    Remove unused [DEFAULT] octavia_plugins
    This option has been unused since amphora v1 was removed [1].
    
    [1] 6c0515c9880b5310dd0647a5881233f07898d812
    
    Change-Id: Ibe52f67f275a0ba90dc43d667f502f0c937e3291
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- octavia/common/config.py
+++ octavia/common/config.py
@@ -41,8 +41,6 @@ core_opts = [
     cfg.HostnameOpt('host', default=utils.get_hostname(),
                     sample_default='<server-hostname.example.com>',
                     help=_("The hostname Octavia is running on")),
-    cfg.StrOpt('octavia_plugins', default='hot_plug_plugin',
-               help=_("Name of the controller plugin to use")),
 ]
 
 api_opts = [
