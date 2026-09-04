ea4e234034b77918d7d42b22996d3dbf1505bc91
Gregory Thiemonge <gthiemon@redhat.com>
2026-09-04 11:07:05 +0200

    Remove DIB_PIP_RETRIES=0 when using CI mirror
    CI mirrors are a bit unstable, DIB fails often when using pip, while pip
    works well from devstack, remove DIB_PIP_RETRIES=0 which disables the
    retries in pip.
    
    Change-Id: Ifbf2456352eb6c2d24e632ffefa0c0a663e7da69
    Signed-off-by: Gregory Thiemonge <gthiemon@redhat.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -112,7 +112,6 @@ function build_octavia_worker_image {
         fi
         export DIB_PYPI_MIRROR_URL=$NODEPOOL_PYPI_MIRROR
         export DIB_PYPI_MIRROR_URL_1=$NODEPOOL_WHEEL_MIRROR
-        export DIB_PIP_RETRIES=0
         export DIB_NO_PYPI_PIP=1
     fi
 
